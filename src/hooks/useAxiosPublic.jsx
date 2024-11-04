bb22fe129e79e59e6cf461f4eb202cd58dc6be56
Antor4q <tariquelislam2015@gmail.com>
2024-11-04 14:07:49 +0600

    static page designed

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosApi = axios.create({
-    // baseURL : 'https://skill-path-server.vercel.app'
-    baseURL : 'http://localhost:5000'
+    baseURL : 'https://skill-path-server.vercel.app'
+    // baseURL : 'http://localhost:5000'
     
 })
 const useAxiosPublic = () => {
